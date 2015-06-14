6dbd7f4b0ea8ec2dc9a4d1ae7fbb7405ecfe9a68
Sam Fok <sam.b.fok@gmail.com>
2015-06-14 16:42:45 -0700

    read header set up data structures

--- parse_prsim.py
+++ parse_prsim.py
@@ -1,4 +1,10 @@
 import argparse
+import numpy as np
+np.set_printoptions(linewidth=1000)
+
+node_names = []
+node_values = []
+node_idx = None
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process the output of prsim')
@@ -6,12 +12,26 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 def read_header(f):
-    while (s=f.readline()) != '(Prsim) cycle':
-        print s
+    """reads in initial statuses"""
+    global node_names, node_values
+    for line in iter(f.readline, ''):
+        if line[:-1] == '(Prsim) cycle':
+            break
+        s = line[:-1].split(' ')
+        if s[0] == '(Prsim)' and s[1] == 'status':
+            status = s[2]
+        else:
+            for n in s[:-1]:
+                node_names.append(n)
+                node_values.append(status)
+    idx = np.argsort(node_names)
+    node_names = np.array(node_names)[idx]
+    node_values = np.array(node_values)[idx]
+    node_idx = {node_names[i]:i for i in xrange(len(node_names))}
     
 if __name__ == "__main__":
     args = parse_args()
     with open(args.filename, 'r') as f:
         read_header(f)
-        
